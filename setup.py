2d3a9083bf56b16e900c64071f6052dfe15fde82
Chris Loew <cloew123@gmail.com>
2015-10-23 14:03:27 -0600

    Added Boilerplate Package code

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='kao_toml',
+      version='0.0.1',
+      description="",
+      author='',
+      author_email='',
+      packages=['kao_toml'],
+     )

896e2b3bc8b3c363291dd44154e4c36659c6b430
Chris Loew <cloew123@gmail.com>
2015-10-25 17:06:31 -0600

    Now properly wrap and collapse around lists

--- kao_toml.py
+++ kao_toml.py
@@ -1,5 +1,7 @@
 from kao_dict import KaoDict
 from kao_decorators import lazy_property, proxy_for
+
+from collections.abc import Mapping, Sequence
 import pytoml
 
 @proxy_for('_toml', ['__iter__', '__contains__', '__len__', '__getitem__', '__setitem__', '__delitem__'])
@@ -37,19 +39,42 @@ class KaoToml(object):
         else:
             setattr(self._toml, attr, value)
             
-    def _wrap(self, d):
-        """ Wrap the given dictionary in a KaoDict """
-        wrappedDict = {}
-        for key, value in d.items():
-            if type(value) is dict:
-                wrappedDict[key] = self._wrap(value)
-            else:
-                wrappedDict[key] = value
+    def _wrap(self, value):
+        """ Wrap the value """
+        if isinstance(value, Mapping):
+            return self._wrapDict(value)
+        elif isinstance(value, Sequence) and not isinstance(value, str) and not isinstance(value, bytes):
+            return self._wrapList(value)
+        else:
+            return value
         
-        return KaoDict(wrappedDict)
+    def _wrapDict(self, d):
+        """ Wrap the dictionary """
+        return KaoDict({key:self._wrap(value) for key, value in d.items()})
+        
+    def _wrapList(self, l):
+        """ Wrap the list """
+        return [self._wrap(v) for v in l]
             
-    def _collapse(self, d):
-        """ Wrap the given dictionary in a KaoDict """
+    def _collapse(self, value):
+        """ Collapse the value """
+        if isinstance(value, Mapping):
+            return self._collapseDict(value)
+        elif isinstance(value, Sequence) and not isinstance(value, str) and not isinstance(value, bytes):
+            return self._collapseList(value)
+        else:
+            return value
+        
+    def _collapseDict(self, d):
+        """ Wrap the dictionary """
+        return {key:self._collapse(value) for key, value in d.items()}
+        
+    def _collapseList(self, l):
+        """ Wrap the list """
+        return [self._collapse(v) for v in l]
+            
+            
+            
         collapsedDict = {}
         for key, value in d.items():
             if type(value) is KaoDict:
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='kao_toml',
-      version='0.0.1',
+      version='0.1.0',
       description="Provides a KaoToml class to wrap a TOML file and allow preserving updates and changes to the file",
       author='Chris Loew',
       author_email='cloew123@gmail.com',
